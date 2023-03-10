910938b40dece1f7017d2539e0dae5bcfd281d59
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-03-10 16:52:23 +0100

    new file:   .env

---
+++ lib/components/payment.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_paystack/flutter_paystack.dart';
+
+class CheckOutPage extends StatefulWidget {
+  const CheckOutPage({Key? key}) : super(key: key);
+
+  @override
+  State<CheckOutPage> createState() => _CheckOutPageState();
+}
+
+class _CheckOutPageState extends State<CheckOutPage> {
+  final payStackClient = PaystackPlugin();
+
+  void _startPaystack() async {
+    await dotenv.load(fileName: '.env');
+    String? publicKey = dotenv.env['PUBLIC_KEY'];
+    payStackClient.initialize(publicKey: publicKey!);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _startPaystack();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Paystack Demo',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CheckOutCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget CheckOutCard() {
+    return InkWell(
+      onTap: () {},
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        color: Colors.blueAccent,
+        elevation: 15,
+        child: Container(
+          height: 100,
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Pay with Paystack",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Icon(
+                  Icons.payment_rounded,
+                  size: 30,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
